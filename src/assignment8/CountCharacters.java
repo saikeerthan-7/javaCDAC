84b0253a72ee59c59684dc6445ea6babafd9ee32
DAC69 <DAC69@ACTS69.ACTS.CDACECBLR.COM>
2026-09-08 15:47:14 +0530

    all files

--- src/assignment8/CountCharacters.java
+++ src/assignment8/CountCharacters.java
@@ -1,5 +1,44 @@
 package assignment8;
+import java.io.*;
+import java.util.*;
 
 public class CountCharacters {
+	
+	public static void main(String[] args) throws IOException {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+		
+		FileWriter fw=new FileWriter("characterfile.txt");
+		
+		
+		fw.write(str);
+		int ch;
+		FileReader fr=new FileReader(str);
+		int upperCase = 0, lowerCase = 0, digits = 0, specialChar = 0, spaces = 0;
+		while((ch=fr.read())!=-1){
+			if(Character.isUpperCase(ch)) {
+				upperCase++;
+			}
+			else if(Character.isLowerCase(ch)) {
+				lowerCase++;
+			}
+			else if (Character.isDigit(ch)){
+				digits++;
+			}
+			else if(Character.isWhitespace(ch)) {
+				spaces++;
+			}
+			else {
+				specialChar++;
+			}
+		}
+		System.out.println("Data Read from file :" + str);
+		System.out.println("UpperCase Characters : " + upperCase);
+		System.out.println("LowerCase Characters : " +lowerCase);
+		sysout
+		
+	}
 
+	
+	
 }
